8ec3553a29244cd85a30b67b1921a19eeaf11999
Diyor Umarkulov <diyor.umarqulov009@gmail.com>
2023-01-29 09:47:29 +0500

    started

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,8 @@
 var main_api_url = "https://api.themoviedb.org/3";
 var api_key = "api_key=d14b1adbe04d8ccc380f0580684188f5";
 var popular_films_url = `${main_api_url}/dicover`;
-
 var header = document.getElementsByTagName("header")[0];
+
 var windowPosition = window.pageYOffset;
 
 window.addEventListener('scroll', scrollingEffect);
