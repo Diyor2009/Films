4d8c6848b3576d621c30e8a35b557339d16bc874
Diyor Umarkulov <diyor.umarqulov009@gmail.com>
2023-01-31 11:55:43 +0500

    Main head is finished

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,14 @@
-var main_api_url = "https://api.themoviedb.org/3";
-var api_key = "api_key=d14b1adbe04d8ccc380f0580684188f5";
+var main_api_url = "https://api.themoviedb.org/3/";
+var api_key = "d14b1adbe04d8ccc380f0580684188f5";
 var popular_films_url = `${main_api_url}/dicover`;
 var header = document.getElementsByTagName("header")[0];
+var res = document.getElementById("res");
 
 var windowPosition = window.pageYOffset;
 
 window.addEventListener('scroll', scrollingEffect);
+
+// fetchAPI(`https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language-ru-RU&page=2`);
 
 function scrollingEffect() {
     let scrollPosition = window.pageYOffset;
@@ -22,3 +25,14 @@ function scrollingEffect() {
 
     windowPosition = scrollPosition;
 };
+
+async function fetchAPI(url) {
+    await fetch(`${url}`)
+    .then(result => result.json())
+    .then(resArray => show(resArray.results))
+}
+
+function show(arr) {
+}
+
+// "Sniper: The White Raven"
