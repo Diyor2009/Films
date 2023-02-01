cecd35360c571d8a39613d09c13c544c13d09fed
Diyor Umarkulov <diyor.umarqulov009@gmail.com>
2023-02-01 23:03:00 +0500

    commit

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,11 @@
-var main_api_url = "https://api.themoviedb.org/3/";
+var main_api_url = "https://api.themoviedb.org/3";
 var api_key = "d14b1adbe04d8ccc380f0580684188f5";
 var header = document.getElementsByTagName("header")[0];
+
+const FILTERS = {
+    trending_this_day: "/trending/all/day?",
+    trending_this_week: "/trending/all/week?",
+}
 
 var listFilters = [
     {
@@ -9,12 +14,37 @@ var listFilters = [
         filterId: "trend",
         filter: [
             {
+                type: "filter",
                 name: "Сегодня",
-                activation: fetchAPI()
-            }
+                // activation: fetchAPI()
+            },
+            {
+                type: "filter",
+                name: "На этой неделе",
+                // activation: fetchAPI()
+            },
+        ]
+    },
+    {
+        filterPosition: 1,
+        filterName: "В тренде",
+        filterId: "hello",
+        filter: [
+            {
+                type: "filter",
+                name: "Hello 1",
+                // activation: fetchAPI()
+            },
+            {
+                type: "filter",
+                name: "Hello 2",
+                // activation: fetchAPI()
+            },
         ]
     },
 ]
+
+showFilters()
 
 var windowPosition = window.pageYOffset;
 window.addEventListener('scroll', scrollingEffect);
@@ -34,17 +64,36 @@ function scrollingEffect() {
     windowPosition = scrollPosition;
 };
 
-fetchAPI(`https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language-ru-RU&page=2`);
+// fetchAPI(FILTERS_API.trending);
+// fetchAPI(FILTERS.trending_this_week);
 
-async function fetchAPI(url, page = 1, results_wrapper) {
-    await fetch(`${main_api_url}${url}`)
-    .then(result => result.json())
-    .then(resArray => show(resArray.results, ))
+// async function fetchAPI(url, page = 1, results_wrapper) {
+//     await fetch(`${main_api_url}${url}api_key=${api_key}&language-ru-RU&page=${page}`)
+//     .then(result => result.json())
+//     .then(obj => showMovie(obj.results, results_wrapper))
+// }
+
+function activateFilter(filterName) {
 }
 
-function activeFilter(filterName) {
-    
+function showFilters() {
+    listFilters.forEach(filter => {
+        let res = document.getElementById(filter.filterId);
+        res.classList.add("list_filter_wrapper");
+        res.innerHTML = `
+            <span class="filter_item active">${filter.filter[0].name}</span>
+            <span class="filter_item">${filter.filter[1].name}</span>
+            <span class="indicator" onclick="activateFilter(\`${filter.filterId}\`)"></span>
+        `
+    })
 }
 
-function show(arr, resutls_wrapper) {
-}
+// function showMovie(arr, resutls_wrapper) {
+//     var res = document.getElementById(resutls_wrapper);
+//     res.innerHTML = "";
+
+//     arr.forEach(movie => {
+//         res.innerHTML += `
+//         `
+//     })
+// }
