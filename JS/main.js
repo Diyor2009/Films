091d66e1886d5933212531f4b9d926f5b889dd0d
Diyor Umarkulov <diyor.umarqulov009@gmail.com>
2023-02-01 19:54:22 +0500

    Commit

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,23 @@
 var main_api_url = "https://api.themoviedb.org/3/";
 var api_key = "d14b1adbe04d8ccc380f0580684188f5";
-var popular_films_url = `${main_api_url}/dicover`;
 var header = document.getElementsByTagName("header")[0];
-var res = document.getElementById("res");
+
+var listFilters = [
+    {
+        filterPosition: 1,
+        filterName: "В тренде",
+        filterId: "trend",
+        filter: [
+            {
+                name: "Сегодня",
+                activation: fetchAPI()
+            }
+        ]
+    },
+]
 
 var windowPosition = window.pageYOffset;
-
 window.addEventListener('scroll', scrollingEffect);
-
-// fetchAPI(`https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language-ru-RU&page=2`);
-
 function scrollingEffect() {
     let scrollPosition = window.pageYOffset;
 
@@ -26,11 +34,17 @@ function scrollingEffect() {
     windowPosition = scrollPosition;
 };
 
-async function fetchAPI(url) {
-    await fetch(`${url}`)
+fetchAPI(`https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language-ru-RU&page=2`);
+
+async function fetchAPI(url, page = 1, results_wrapper) {
+    await fetch(`${main_api_url}${url}`)
     .then(result => result.json())
-    .then(resArray => show(resArray.results))
+    .then(resArray => show(resArray.results, ))
 }
 
-function show(arr) {
+function activeFilter(filterName) {
+    
+}
+
+function show(arr, resutls_wrapper) {
 }
